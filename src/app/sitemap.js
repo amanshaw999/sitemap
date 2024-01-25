47a32f9cfb48cca3a409bd9defd93463f06fad47
Amanshaw9010 <aman.mesopet@outlook.com>
2024-01-25 08:26:14 +0530

    added new server origin

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,7 +2,7 @@ export default async function sitemap() {
     const { data } = await (await fetch('http://localhost:4000/blogs')).json();
 
     return data.map(blog => ({
-        url: `http://localhost:3000/blog/${blog.title}`,
+        url: `https://mint-advanced-tmp-mounts.trycloudflare.com/blog/${blog.title}`,
         lastModified: new Date(blog.publishAt).toLocaleDateString(),
     }));
 }
