f7f1e313830b2c59ab175eff2f04a87913943cf1
Amanshaw9010 <aman.mesopet@outlook.com>
2024-01-25 08:55:28 +0530

    added new server origin

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,10 @@
 export default async function sitemap() {
-    const { data } = await (await fetch('http://localhost:4000/blogs')).json();
+    const { data } = await (
+        await fetch('https://server-olth.vercel.app/blogs')
+    ).json();
 
     return data.map(blog => ({
-        url: `https://mint-advanced-tmp-mounts.trycloudflare.com/blog/${blog.title}`,
+        url: `http://localhost:3000/blog/${blog.title}`,
         lastModified: new Date(blog.publishAt).toLocaleDateString(),
     }));
 }
