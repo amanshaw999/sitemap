3bcf4f84b9aeb2f986596360b4535f651784b62f
Amanshaw9010 <aman.mesopet@outlook.com>
2024-01-25 08:23:37 +0530

    api changed

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,7 +2,7 @@ export default async function sitemap() {
     const { data } = await (await fetch('http://localhost:4000/blogs')).json();
 
     return data.map(blog => ({
-        url: `${window.location.host}/blog/${blog.title}`,
+        url: `http://localhost:3000/blog/${blog.title}`,
         lastModified: new Date(blog.publishAt).toLocaleDateString(),
     }));
 }
